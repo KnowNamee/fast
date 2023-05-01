f0538e71f1d06f6b54d0b3241fd1432d1761d216
Ivan Stupakevich <st.onlyemail@gmail.com>
2023-05-02 00:02:19 +0300

    Add TASSpinLock impl

--- fast/include/sync/spinlock.h
+++ fast/include/sync/spinlock.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <types.h>
+
+#include <atomic>
+
 namespace fast::sync {
+
+class TASSpinLock {
+ public:
+    void lock();
+    void unlock();
+
+ private:
+    std::atomic<u32> _atomic = 0;
+};
 
 }
--- fast/src/sync/spinlock.cc
+++ fast/src/sync/spinlock.cc
@@ -1,5 +1,22 @@
 #include <sync/spinlock.h>
 
+#include <thread>
+
 namespace fast::sync {
+
+void TASSpinLock::unlock() {
+    while (true) {
+        if (!_atomic.exchange(1, std::memory_order_acquire)) {
+            return;
+        }
+        while (_atomic.load(std::memory_order_relaxed)) {
+            std::this_thread::yield();
+        }
+    }
+}
+
+void TASSpinLock::lock() {
+    _atomic.exchange(0, std::memory_order_release);
+}
 
 }
