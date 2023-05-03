700a8720537deebfddd74f5992a9166752f84aef
Ivan Stupakevich <st.onlyemail@gmail.com>
2023-05-03 22:38:46 +0300

    Improve mutex correctness

--- fast/src/sync/mutex.cc
+++ fast/src/sync/mutex.cc
@@ -7,20 +7,20 @@ namespace fast::sync {
 
 void Mutex::lock() {
     u32 val = 0;
-    if (!_atomic.compare_exchange_strong(val, 1, std::memory_order_acquire)) {
+    if (!_atomic.compare_exchange_strong(val, 1, std::memory_order_acq_rel)) {
         if (val != 2) {
-            val = _atomic.exchange(2, std::memory_order_relaxed);
+            val = _atomic.exchange(2, std::memory_order_acquire);
         }
         while (val != 0) {
             futexWait(_atomic, 2, std::memory_order_relaxed);
-            val = _atomic.exchange(2, std::memory_order_relaxed);
+            val = _atomic.exchange(2, std::memory_order_acquire);
         }
     }
 }
 
 void Mutex::unlock() {
     if (_atomic.fetch_sub(1, std::memory_order_release) != 1) {
-        _atomic.store(0, std::memory_order_relaxed);
+        _atomic.store(0, std::memory_order_release);
         futexWake(futexAddr(_atomic), 1);
     }
 }
